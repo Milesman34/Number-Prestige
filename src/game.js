0a6d3f1ddf245bd2d59c805a148814391e1072b2
milesman34 <miles.saunders@icloud.com>
2019-11-17 16:50:39 -0500

    Added header items

--- src/game.js
+++ src/game.js
@@ -1,6 +1,14 @@
+//Removes the identifier from the element name
+const removeIdentifier = elementName => ["#", "."].includes(elementName[0]) ?
+    elementName.slice(1, elementName.length) :
+    elementName;
+
 //List of themes
 //CSS class should be [element]-[theme-name]
 const themes = ["light", "dark"];
+
+//Elements to modify
+const elements = ["#app", "#header", ".header-item"]
 
 //Game constructor (only one at a time)
 const Game = ({theme = "dark"} = {}) => {
@@ -10,13 +18,17 @@ const Game = ({theme = "dark"} = {}) => {
         setTheme(theme) {
             this.theme = theme;
 
-            //Removes all themes from the class
+            //Removes all themes from each element
             themes.forEach(theme => {
-                $("#app").removeClass(`app-${theme}`);
+                elements.forEach(element => {
+                    $(element).removeClass(`${removeIdentifier(element)}-${theme}`);
+                });
             });
 
             //Adds the required themes
-            $("#app").addClass(`app-${this.theme}`);
+            elements.forEach(element => {
+                $(element).addClass(`${removeIdentifier(element)}-${this.theme}`);
+            });
         }
     };
 
