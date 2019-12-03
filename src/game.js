f82186e453dea1a995d7538932935e3119500854
milesman34 <miles.saunders@icloud.com>
2019-12-02 20:43:19 -0500

    Some gameplay

--- src/game.js
+++ src/game.js
@@ -12,6 +12,15 @@ let app = new Vue({
         //Current open selector
         selector: "none",
 
+        //Current score
+        score: 0,
+
+        //Current prestige goal
+        goal: 10,
+
+        //Current amount gained on click
+        gain: 1,
+
         //Current save file
         currentSaveFile: 0,
 
@@ -29,6 +38,23 @@ let app = new Vue({
         //Generates the theme class used for a given element
         themeClass(elem) {
             return `${elem}-${this.theme}`;
+        },
+
+        //Rounds a number to a given number of decimal places
+        roundTo(num, places = 0) {
+        	return Math.round(num * 10 ** places) / (10 ** places);
+        },
+
+        //Formats a number in exponential format (if over the requirement)
+        formatSci(num, limit = 1e6, places = 2) {
+        	if (num >= limit) {
+                //Splits the orders of magnitude and the amount before it to format it
+        		let oom = Math.floor(Math.log10(num));
+        		let div = this.roundTo(num / (10 ** oom), places);
+
+        		return `${div}e${oom}`;
+        	} else
+        		return num.toString();
         },
 
         //SETTERS
@@ -50,6 +76,15 @@ let app = new Vue({
             this.selector = selector;
         },
 
+        //Updates the score
+        setScore(score) {
+            this.score = score;
+        },
+
+        addScore(score) {
+            this.setScore(this.score + score);
+        },
+
         //SAVE FILE FUNCTIONS
         //Attempts to load a value from localStorage, replacing it with a default value if it does not exist
         lsGetOrSetDefault(item, def) {
@@ -61,7 +96,7 @@ let app = new Vue({
 
         //Encodes the current save data
         encodeSaveData() {
-            return `${this.theme}|${this.state}`;
+            return `${this.theme}|${this.state}|${this.score}|${this.goal}|${this.gain}`;
         },
 
         //Sets a save file
@@ -79,6 +114,9 @@ let app = new Vue({
 
             this.setTheme(data[0]);
             this.setState(data[1]);
+            this.setScore(data.length >= 3 ? parseInt(data[2]) : 0);
+            this.goal = data.length >= 4 ? parseInt(data[3]) : 10;
+            this.gain = data.length >= 5 ? parseInt(data[4]) : 1;
         },
 
         //Saves the game in the current save slot
@@ -97,6 +135,9 @@ let app = new Vue({
                 //Resets all values
                 this.setTheme("dark");
                 this.setState("main");
+                this.setScore(0);
+                this.goal = 10;
+                this.gain = 1;
 
                 //Saves over save file
                 this.save();
