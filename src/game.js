eeec9d5e5fcbebef31aabe120e7686d84cee619d
milesman34 <miles.saunders@icloud.com>
2019-12-02 21:28:22 -0500

    Added 2 more upgrades

--- src/game.js
+++ src/game.js
@@ -39,6 +39,30 @@ let app = new Vue({
                 boost() {
                     return 2 ** this.amount;
                 }
+            },
+
+            //Number gain upgrade
+            {
+                cost: 4,
+                costScaling: 5,
+
+                amount: 0,
+
+                boost() {
+                    return this.amount;
+                }
+            },
+
+            //Reduce requirement upgrade
+            {
+                cost: 5,
+                costScaling: 5,
+
+                amount: 0,
+
+                boost() {
+                    return 0.9 ** this.amount;
+                }
             }
         ],
 
@@ -75,7 +99,7 @@ let app = new Vue({
 
         		return `${div}e${oom}`;
         	} else
-        		return num.toString();
+        		return this.roundTo(num, places).toString();
         },
 
         //Returns the correct plural ending (s ending)
@@ -114,6 +138,16 @@ let app = new Vue({
         //Gets the current prestige point gain
         prestigePointGain() {
             return this.upgrades[0].boost();
+        },
+
+        //Actual number gain
+        actualGain() {
+            return this.gain + this.upgrades[1].boost();
+        },
+
+        //Actual prestige goal
+        actualGoal() {
+            return this.goal * this.upgrades[2].boost();
         },
 
         //GAME STUFF
@@ -163,7 +197,7 @@ let app = new Vue({
 
         //Encodes the current save data
         encodeSaveData() {
-            return `${this.theme}|${this.state}|${this.score}|${this.goal}|${this.gain}|${this.prestiges}|${this.prestigePoints}|${this.upgrades[0].cost}|${this.upgrades[0].amount}`;
+            return `${this.theme}|${this.state}|${this.score}|${this.goal}|${this.gain}|${this.prestiges}|${this.prestigePoints}|${this.upgrades[0].cost}|${this.upgrades[0].amount}|${this.upgrades[1].cost}|${this.upgrades[1].amount}|${this.upgrades[2].cost}|${this.upgrades[2].amount}`;
         },
 
         //Sets a save file
@@ -188,6 +222,10 @@ let app = new Vue({
             this.prestigePoints = data.length >= 7 ? parseInt(data[6]) : 0;
             this.upgrades[0].cost = data.length >= 8 ? parseInt(data[7]) : 2;
             this.upgrades[0].amount = data.length >= 9 ? parseInt(data[8]) : 0;
+            this.upgrades[1].cost = data.length >= 10 ? parseInt(data[9]) : 4;
+            this.upgrades[1].amount = data.length >= 11 ? parseInt(data[10]) : 0;
+            this.upgrades[2].cost = data.length >= 12 ? parseInt(data[11]) : 5;
+            this.upgrades[2].amount = data.length >= 13 ? parseInt(data[12]) : 0;
         },
 
         //Saves the game in the current save slot
@@ -213,6 +251,10 @@ let app = new Vue({
                 this.prestigePoints = 0;
                 this.upgrades[0].cost = 2;
                 this.upgrades[0].amount = 0;
+                this.upgrades[1].cost = 4;
+                this.upgrades[1].amount = 0;
+                this.upgrades[2].cost = 5;
+                this.upgrades[2].amount = 0;
 
                 //Saves over save file
                 this.save();
