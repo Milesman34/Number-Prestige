c3afcc60009b837a399f7cf166a9fda583b0f17c
milesman34 <miles.saunders@icloud.com>
2019-12-27 23:43:45 -0500

    themeClass is better implemented and used in component

--- src/components/ThemeSelectorListButton.js
+++ src/components/ThemeSelectorListButton.js
@@ -1,10 +1,17 @@
 Vue.component("theme-selector-list-button", {
+    mixins: [utils],
+
     //currentTheme represents the game's current theme
     props: ["theme", "currentTheme"],
 
-    mixins: [utils],
+    methods: {
+        //Returns the current theme class
+        currentThemeClass(elem) {
+            return this.themeClass(elem, this.currentTheme);
+        }
+    },
 
     template: `
-        <button class="theme-selector-list-button" v-bind:class="[themeClass('selector-item', currentTheme), themeClass('selector-hover', currentTheme), themeClass('text', currentTheme)]" v-on:click="$emit('set-theme', theme)">{{ capitalize(theme) }}</button>
+        <button class="theme-selector-list-button" v-bind:class="[currentThemeClass('selector-item'), currentThemeClass('selector-hover'), currentThemeClass('text')]" v-on:click="$emit('set-theme', theme)">{{ capitalize(theme) }}</button>
     `
 })
