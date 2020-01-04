21bb47583a25e3fcb637ff2527b5b3ba86f55c9f
milesman34 <miles.saunders@icloud.com>
2020-01-04 00:35:19 -0500

    Improved UpgradeButton component

--- src/components/UpgradeButton.js
+++ src/components/UpgradeButton.js
@@ -2,16 +2,15 @@ Vue.component("upgrade-button", {
     mixins: [utils, storeIO],
 
     props: [
-        "id",
         "description",
         "formatFunction",
         "boost",
         "cost",
-        "canAfford"
+        "affordable"
     ],
 
     template: `<div class="prestige-upgrade-container">
-        <button class="prestige-upgrade-button" v-bind:class="[getThemeClass('game-button'), canAfford(id) ? '' : getThemeClass('unaffordable')]" v-on:click="$emit('buy-upgrade', id)">
+        <button class="prestige-upgrade-button" v-bind:class="[getThemeClass('game-button'), affordable ? '' : getThemeClass('unaffordable')]" v-on:click="$emit('event')">
             <div class="prestige-upgrade-desc" v-bind:class="getThemeClass('text')">{{ description }}</div>
             <div class="prestige-upgrade-current" v-bind:class="getThemeClass('text')">Currently: {{ formatFunction(boost) }}</div>
             <div class="prestige-upgrade-cost" v-bind:class="getThemeClass('text')">Cost: {{ cost }} Prestige Points</div>
